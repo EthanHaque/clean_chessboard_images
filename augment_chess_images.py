b5408b410e4aa2c41388054d19e7c799a9deb911
Ethan Haque <43977335+EthanHaque@users.noreply.github.com>
2021-04-05 19:53:53 -0500

    initial commit

---
+++ augment_chess_images.py
@@ -0,0 +1,12 @@
+import augmentor
+import os
+
+
+def get_image_paths(directory_path):
+    directory_contents = os.listdir(directory_path)
+
+    return directory_contents
+
+
+def main():
+    pass
