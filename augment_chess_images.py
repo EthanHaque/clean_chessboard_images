011cf0d09b8cd12afa78fdaa4ef2182cf7e8d525
Ethan Haque <43977335+EthanHaque@users.noreply.github.com>
2021-04-10 21:45:37 -0500

    adding comments

--- augment_chess_images.py
+++ augment_chess_images.py
@@ -19,6 +19,13 @@ def get_image_paths(directory_path, extensions=None):
 
 
 def random_corner_warp(image, strength=0.2):
+    """
+    Randomly warps the corners of an image.
+
+    :param image: The image to warp.
+    :param strength: The amount of distortion.
+    :return: The image with corners warped.
+    """
     choice = np.random.rand()
     x_scale_factors = ag.get_rand_points(low=0, high=strength, shape=[4])
     y_scale_factors = ag.get_rand_points(low=0, high=strength, shape=[4])
@@ -34,6 +41,13 @@ def random_corner_warp(image, strength=0.2):
 
 
 def random_translation(image, strength=0.1):
+    """
+    Randomly shifts an image.
+
+    :param image: The image to translate.
+    :param strength: The amount of distortion.
+    :return: The image with translations applied.
+    """
     rows, cols = image.shape[0:2]
 
     distance_scale_factor = (np.random.rand() - 0.5) * strength
@@ -43,6 +57,14 @@ def random_translation(image, strength=0.1):
 
 
 def random_rotation(image, strength=1, resize=True):
+    """
+    Rotates an image around the center.
+
+    :param image: The image to rotate
+    :param strength: The amount of distortion
+    :param resize: resize the output image to not crop the image.
+    :return: The rotated image.
+    """
     angle = (2 * np.random.rand() - 1) * 360 * strength
 
     rotated = ag.rotate(image, angle, resize)
@@ -51,6 +73,14 @@ def random_rotation(image, strength=1, resize=True):
 
 
 def random_blur(image, standard_deviation=3, strength=3):
+    """
+    Randomly apply a blur to an image.
+
+    :param image: The image to blur
+    :param standard_deviation: How many standard deviations from the mean a gaussian blur should use.
+    :param strength: The amount of blur.
+    :return: The blurred image.
+    """
     choice = np.random.rand()
 
     if choice >= 0.66:
@@ -64,6 +94,14 @@ def random_blur(image, standard_deviation=3, strength=3):
 
 
 def random_noise(image, standard_deviation=3, strength=0.1):
+    """
+    Adds random noise to an image.
+
+    :param image: The image to add noise to.
+    :param standard_deviation: How many standard deviations from the mean the noise function should use.
+    :param strength: The amount of noise.
+    :return: The noisy image.
+    """
     choice = np.random.rand()
 
     if choice >= 0.5:
@@ -75,6 +113,14 @@ def random_noise(image, standard_deviation=3, strength=0.1):
 
 
 def random_distortion(image, standard_deviation=5, strength=0.4):
+    """
+    Randomly distorts an image with either 1D or 2D distortions.
+
+    :param image: The image to distort.
+    :param standard_deviation: How many standard deviations from the mean the warping functions should use.
+    :param strength: The amount of distortion.
+    :return: The distorted image.
+    """
     choice = np.random.rand()
 
     strength *= 10
@@ -92,6 +138,7 @@ def main():
 
 if __name__ == '__main__':
     paths = get_image_paths("./data/sub_set/train")
+
     image = random_distortion(ag.load(paths[-10]))
     plt.imshow(image)
     plt.show()
